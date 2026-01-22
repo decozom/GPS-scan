031fb2112bc02088d7762c21855023929562374c
decozom <samuhai000@gmail.com>
2026-01-22 21:47:52 +0900

    sw.js を作成

---
+++ sw.js
@@ -0,0 +1,23 @@
+const CACHE_NAME = "pingpon-v1";
+const ASSETS = [
+  "./",
+  "./index.html",
+  "./manifest.json",
+  "./icons/icon-192.png",
+  "./icons/icon-512.png",
+  "./fields/fields.json",
+  "./fields/oasis.json",
+  "./maps/oasis.jpg"
+];
+
+self.addEventListener("install", e => {
+  e.waitUntil(
+    caches.open(CACHE_NAME).then(cache => cache.addAll(ASSETS))
+  );
+});
+
+self.addEventListener("fetch", e => {
+  e.respondWith(
+    caches.match(e.request).then(res => res || fetch(e.request))
+  );
+});
